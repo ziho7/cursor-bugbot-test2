4f7190af77870393174f5a5db409e0d0bd810870
ziho7 <78910421@qq.com>
2025-06-05 16:57:57 +0800

    feat change div

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-CN">
-      <body
+      <bod
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
